15f4feff2bcd74f5de287780ebcc64c2207def42
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-05 11:29:55 +0800

    did migration but dont know if correct

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,2 +1,10 @@
 // todo
-// images
+// implement images
+// - need to create image schema
+// - create image endpoint
+// - do form for images
+
+// ISSUES
+// migrating schema. when i do npx prisma migrate dev, it didnt know which env to use - the env test or env development.
+// so i when to package.json and added the migrate:dev thingy and specified env dev.
+// now to migrate, i had to run npm migrate:dev, and it worked
